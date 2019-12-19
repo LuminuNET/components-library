d16ad4db6458f67b1e9a5d35196403b582b03c69
Keimeno <constantin@metzworld.com>
2019-12-19 23:13:30 +0100

    [added] basic entry point

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import i18n from './translations';
 
 Vue.config.productionTip = false;
+
+const vm = new Vue({
+	router,
+	i18n,
+	render: h => h(App)
+}).$mount('#app');
+
+export { vm };
