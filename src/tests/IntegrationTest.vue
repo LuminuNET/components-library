d18a4c5a76663c8ffebbf312b3bf310c2539c56e
Keimeno <constantin@metzworld.com>
2019-12-19 23:12:25 +0100

    [added] integration test

---
+++ src/tests/IntegrationTest.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<lm-header :image="require('@/assets/style-logo.png')" />
+		<lm-sticky-header :links="links" />
+		<div class="view container">
+			<lm-card class="fake-content"></lm-card>
+		</div>
+		<lm-footer />
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import LmHeader from "@/components/layout/Header.vue";
+import LmStickyHeader from "@/components/layout/StickyHeader.vue";
+import LmFooter from "@/components/layout/Footer.vue";
+import LmCard from "@/components/base/Card.vue";
+
+export default Vue.extend({
+	components: {
+		LmHeader,
+		LmStickyHeader,
+		LmCard,
+		LmFooter
+	},
+	data: () => ({
+		links: [
+			{
+				name: "overview",
+				to: "/",
+				isExternal: false,
+				hasChildren: false
+			},
+			{
+				name: "test",
+				to: "https://example.org",
+				isExternal: true,
+				hasChildren: false
+			}
+		]
+	})
+});
+</script>
+
+<style lang="scss" scoped>
+.fake-content {
+	height: 400px;
+	margin-bottom: 30px;
+}
+</style>
