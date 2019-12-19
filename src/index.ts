c4a2fc70991bad2cc5f6de66d67428310b5cb8a6
Keimeno <constantin@metzworld.com>
2019-12-19 23:15:55 +0100

    [added] export for footer

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as LmCard } from '@/components/base/Card.vue';
 // Layout components
 export { default as LmHeader } from '@/components/layout/Header.vue';
 export { default as LmStickyHeader } from '@/components/layout/StickyHeader.vue';
+export { default as LmFooter } from '@/components/layout/Footer.vue';
